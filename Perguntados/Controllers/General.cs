558e7a04ed12efe9d01009e7b92191f0b791b210
Klaupher <fernanda.j.bernardes@avanade.com>
2022-02-16 10:25:18 -0300

    Criacao de Menu

---
+++ Perguntados/Controllers/General.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Perguntados.Helpers;
+using System.Text.Json;
+
+namespace Perguntados.Controllers
+{
+    public class General
+    {
+        private string _fileName = "C:\\Projetos\\Treinamento\\Perguntados\\Perguntados\\Data\\banco.json";
+        public string? InserePergunta(Pergunta pergunta)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+            try
+            {
+                List<Pergunta>? perguntas = JsonSerializer.Deserialize<List<Pergunta>>(Helper.GetJSON(_fileName), options);
+                perguntas.Add(pergunta);
+                Helper.SetJSON(_fileName, JsonSerializer.Serialize(perguntas));
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return null;
+
+        }
+    }
+}
--- Perguntados/Program.cs
+++ Perguntados/Program.cs
@@ -5,17 +5,33 @@ namespace Perguntados
 {
     internal class Program
     {
+        private static readonly string _filename = "C:\\Projetos\\Treinamento\\Perguntados\\Perguntados\\Data\\banco.json";
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            var fileJson = "C:\\Projetos\\Treinamento\\Perguntados\\Perguntados\\Data\\banco.json";
-            var jsonString = File.ReadAllText(fileJson);
-            var options = new JsonSerializerOptions
+            while (true)
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true,
-            };
-            List<Pergunta>? perguntas = JsonSerializer.Deserialize<List<Pergunta>>(jsonString, options);
+                var escolha = MenuUsuario();
+                if (escolha == 4)
+                    return;
+                switch (escolha) {
+                    case 1: ListaPerguntas();
+                        break;
+                }
+                
+            }
+            
+            
+        }
+
+        static void ListaPerguntas()
+        {
+            List<Pergunta>? perguntas = JsonSerializer.Deserialize<List<Pergunta>>(Helpers.Helper.GetJSON(_filename), _options);
 
             if (perguntas.Any())
             {
@@ -24,7 +40,26 @@ namespace Perguntados
                     Console.WriteLine($"{perg.Id} - {perg.Questao}");
                 }
             }
-            
+            Console.WriteLine();
+        }
+        static int MenuUsuario()
+        {
+            Console.WriteLine("******** SELECIONE UMA OPÇÃO ***********");
+            Console.WriteLine();
+            Console.WriteLine("1 - Listar todas as perguntas");
+            Console.WriteLine("2 - Inserir nova pergunta");
+            Console.WriteLine("3 - Excluir uma pergunta");
+            Console.WriteLine("4 - Sair");
+            Console.Write("Digite sua opção: ");
+            try
+            {
+                int resp = Convert.ToInt32(Console.ReadLine());
+                return resp;
+            }
+            catch (Exception)
+            {
+                return 4;
+            }
         }
     }
 }
